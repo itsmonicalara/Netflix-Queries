068c26d7222abb84cd75b2157a5342b1ec9e5767
Monica Lara <A01655306@itesm.mx>
2020-11-24 21:14:59 -0600

    Final

--- apiMongo.py
+++ apiMongo.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 import redis
-import datetime
 from bson.objectid import ObjectId
+from datetime import date
+
 
 client = MongoClient()
 client = MongoClient('localhost', 27017)
@@ -31,26 +32,24 @@ def movie_name(movie):
 					"Country: " + x["country"] + "\n" + \
 					"Release year: " + x["release_year"]
 				key = movie
-				# Redis cache
 				r.set(key, info)
-				# Idk about the time
 				r.expire(key, "300")
 				print(info)
 
 # Given an actor name - Obtain a list with the movies and a list with the TV shows where he/she has participated.
 def lists_of_actor(actor):
-    try:
-        key = actor
-        info = r.get(key).decode("utf-8")
-    except:
-        info = ""
-        key = actor
-        my_query = collection.find({"cast": {"$regex": actor}})
-        for x in my_query:
-            info += x["title"]+"\n"
-        print(info)
-        r.set(key, info)
-        r.expire(key, "300")
+	try:
+		key = actor
+		info = r.get(key).decode("utf-8")
+	except:
+		info = ""
+		key = actor
+		my_query = collection.find({"cast": {"$regex": actor}})
+		for x in my_query:
+			info += x["title"]+"\n"
+		print(info)
+		r.set(key, info)
+		r.expire(key, "300")
 
 # Given a TV show name - Obtain the director cast, countries, and release year.
 def tvShow_name(tvShow):
@@ -91,29 +90,26 @@ def total_tv_shows():
 
 # Total number of movies for a given country.
 def total_movies_country(country):
-    try:
-        key = country
-        r.get(key).decode("utf-8")
-    except:
-        my_doc = collection.count_documents({"country": country, "type": "Movie"})
-        key = country
-        r.set(key, my_doc)
-        r.expire(key, "300")
-        print("Total number of movies", my_doc)
-
-
+	try:
+		key = country
+		r.get(key).decode("utf-8")
+	except:
+		my_doc = collection.count_documents({"country": country, "type": "Movie"})
+		key = country
+		r.set(key, my_doc)
+		r.expire(key, "300")
+		print("Total number of movies", my_doc)
 
 # Total number of TV shows for given release year.
 def total_tv_shows_by_year(release_year):
 	try:
-        key = release_year
-        r.get(key).decode("utf-8")
-    except:
-        my_doc = collection.count_documents({"release_year": release_year,"type": "TV Show"})
-        r.set(key, my_doc)
-        r.expire(key, "300
-        print("Total number of TV shows of ",release_year," are: ",my_doc)
-
+		key = release_year
+		r.get(key).decode("utf-8")
+	except:
+		my_doc = collection.count_documents({"release_year": release_year,"type": "TV Show"})
+		r.set(key, my_doc)
+		r.expire(key, "300")
+		print("Total number of TV shows of ",release_year," are: ",my_doc)
 
 # One query to add or update an entity in the database
 # Add a new movie
@@ -155,9 +151,9 @@ def add_tvShow(title, director, cast, country, date_added, release_year, rating,
 
 
 if __name__ == '__main__':
-	ans1=True
+	ans1 = True
 	while ans1:
-    	print ("""
+		print ("""
 		1. Given a movie name - Obtain the director cast, countries and release year
 		2. Given an actor name - Obtain a list with the movies and a list with the TV shows where he/she has participated.
 		3. Given a TV show name - Obtain the director cast, countries, and release year.
@@ -168,28 +164,48 @@ if __name__ == '__main__':
 		8. Add a new TV show
 		9. Exit
 		""")
-		ans=raw_input("What would you like to do? ") 
-		if ans=="1":
-			ans = input("movie name?") 
+		ans = int(input("What would you like to do?\n"))
+		if ans == 1:
+			ans = input("Movie name?\n") 
 			movie_name(ans)
-		elif ans=="2":
-			ans = input("actor name?") 
+		elif ans== 2:
+			ans = input("Actor name?\n") 
 			lists_of_actor(ans)
-		elif ans=="3":
-			ans = input("TV Show name?")
+		elif ans== 3:
+			ans = input("TV Show name?\n")
 			tvShow_name(ans)
-		elif ans=="4":
+		elif ans== 4:
 			total_tv_shows()
-		elif ans=='5':
-			ans = input("Country?")
+		elif ans== 5:
+			ans = input("Country?\n")
 			total_movies_country(ans)
-		elif ans=='6':
-			ans = input("year?")
+		elif ans== 6:
+			ans = input("Year?\n")
 			total_tv_shows_by_year(ans)
-		elif ans=='7':
-
-		elif ans=='8':
-		
-		else ans=="9":
+		elif ans== 7:
+			title = input("Name of the movie:\n")
+			director = input("Director:\n")
+			cast = input("Movie cast:\n")
+			country = input("Name of the country:\n")
+			date_added = now.strftime("%B %d, %Y")
+			release_year = input("Release year:\n")
+			rating = input("Rating:\n")
+			duration = input("Duration:\n")
+			listed_in = input("Listed in:\n")
+			description = input("Description\n")
+			add_movie(title, director, cast, country, date_added, release_year, rating, duration, listed_in, description)
+		elif ans== 8:
+			title = input("Name of the TV Show:\n")
+			director = input("Director:\n")
+			cast = input("Movie cast:\n")
+			country = input("Name of the country:\n")
+			date_added = now.strftime("%B %d, %Y")
+			release_year = input("Release year:\n")
+			rating = input("Rating:\n")
+			duration = input("Duration:\n")
+			listed_in = input("Listed in:\n")
+			description = input("Description\n")
+			add_tvShow(title, director, cast, country, date_added, release_year, rating, duration, listed_in, description)
+		else:
 			ans1 = False
 		
